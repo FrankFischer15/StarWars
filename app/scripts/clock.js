3b094ca045ff7d52dcea29156dd86287d31aad01
Derick Warshaw <derickwarshaw@gmail.com>
2015-02-13 20:17:01 -0500

    Updating basically everything that lead up to presentation of web app

---
+++ app/scripts/clock.js
@@ -0,0 +1,19 @@
+		var clock;
+
+		$("#start").click(function() {
+		var clock;
+		clock = $('#timer').FlipClock({
+		    clockFace: 'counter',
+		    autoStart: true,
+
+		    
+		});
+
+		clock.setTime(0);
+
+		
+		
+
+
+
+		});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,41 @@
-'use strict'
+var layout = $("<div class='container-fluid'><div class='row gameTitle'><div class='col-xs-3'><img src='images/gamelogo.png' /></div><div class='col-xs-2'><button type='button' id='start'>START</button></div><div class='row'><div id='timer'></div></div><div class='col-xs-1'><div id='counter'></div><div id='wrong'></div></div><div class='col-xs-3 choiceOne'></div><div class='col-xs-3 choiceTwo'></div></div><div class='row gameArea'><div class='col-xs-12 cardSpace' id='insertCards'></div></div></div>")
+var cardsArea = $("<div class='row'><div class='col-xs-2' id='t0'></div><div class='col-xs-2' id='t1'></div><div class='col-xs-2' id='t2'></div><div class='col-xs-2' id='t3'></div><div class='col-xs-2' id='t4'></div><div class='col-xs-2' id='t5'></div></div><div class='row'><div class='col-xs-2' id='t6'></div><div class='col-xs-2' id='t7'></div><div class='col-xs-2' id='t8'></div><div class='col-xs-2' id='t9'></div><div class='col-xs-2' id='t10'></div><div class='col-xs-2' id='t11'></div></div><div class='row'><div class='col-xs-2' id='t12'></div><div class='col-xs-2' id='t13'></div><div class='col-xs-2' id='t14'></div><div class='col-xs-2' id='t15'></div><div class='col-xs-2' id='t16'></div><div class='col-xs-2' id='t17'></div></div><div class='row'><div class='col-xs-2' id='t18'></div><div class='col-xs-2' id='t19'></div><div class='col-xs-2' id='t20'></div><div class='col-xs-2' id='t21'></div><div class='col-xs-2' id='t22'></div><div class='col-xs-2' id='t23'></div></div><div class='row'><div class='col-xs-2' id='t24'></div><div class='col-xs-2' id='t25'></div><div class='col-xs-2' id='t26'></div><div class='col-xs-2' id='t27'></div><div class='col-xs-2' id='t28'></div><div class='col-xs-2' id='t29'></div></div>")
 
 
 
-var back = 'images/troll.png';
+var back = 'images/death_star.png';
 var tile = ['images/vader.png', 'images/ackbar.png', 'images/boba_fett.png', 'images/C3PO.png', 'images/chewbacca.png', 'images/han_solo.png',
     'images/jar_jar.png', 'images/lando.png', 'images/leia.png', 'images/luke_skywalker.png', 'images/obi_wan.png', 'images/qui_gon.png',
     'images/R2D2.png', 'images/yoda.png', 'images/anakin.png'
 ];
 
 
+var totalMoves = 0;
+var wrongMoves = 0;
+
+build();
+cards();
+
+
+
+
+function build() {
+    $("body").append(layout);
+
+}
+
+
+
+function cards() {
+    $("#insertCards").append(cardsArea);
+}
+
 /* randomizeOrder function randomizes the board of cards using some helpful jQuery functions*/
 function randomizeOrder(a, b) {
     return (Math.round(Math.random()) - 0.5);
 }
 
-/* assignment of the tiles location to the images.*/ 
+/* assignment of the tiles location to the images.*/
 var card = [];
 for (var counter = 0; counter < 15; counter++) {
     card[counter] = new Image();
@@ -35,8 +56,8 @@ function displayBack(counter) {
 
 /* initialize the game by starting on load with all backs of the cards facing the user.
 Set timer to start at 0:00 and set timer interval to 1 second
-*/ 
-var choice1, choice2, timer, tileNo, tileId, count, timeId;
+*/
+var choice1, choice2, timer, numCardsSelected, tileId, count, timeId;
 window.onload = start;
 
 
@@ -44,14 +65,14 @@ window.onload = start;
 function start() {
     for (var counter = 0; counter <= 29; counter++) displayBack(counter);
 
-    
+
     clearInterval(tileId);
-    timer = tileNo = timeId = 0;
+    timer = numCardsSelected = timeId = 0;
 
     // sort method sorts items in the array of tile objects the tiles randomly
     tile.sort(randomizeOrder);
 
-    
+
 
 }
 
@@ -60,28 +81,41 @@ working with the flipping effect. If this is the first card flipped it will wait
 card being selected it will compare selection 1 and 2 and then act based on logic
 */
 function disp(selection) {
-    if (tileNo > 1) {
+    if (numCardsSelected > 1) {
         clearTimeout(count);
         conceal();
     }
     // this deals with the tile array element selected and using logic determines action upon it
     document.getElementById('t' + selection).innerHTML = tile[selection];
-    if (tileNo == 0) choice1 = selection;
+    if (numCardsSelected == 0) choice1 = selection;
     else {
         choice2 = selection;
         count = setTimeout('conceal()', 900);
     }
-    tileNo++;
+    numCardsSelected++;
 }
 
 /* conceal() works with the 2 selections the user makes and compares them to determine
 if they are a match or not - if not it sends those selections to have them returned to displaying
 their backside to the user */
 function conceal() {
-    tileNo = 0;
+    numCardsSelected = 0;
     if (tile[choice1] != tile[choice2]) {
         displayBack(choice1);
+
         displayBack(choice2);
-    } else timeId++;
-    if (timeId >= 15) clearInterval(tileId);
+
+        totalMoves++;
+        wrongMoves++;
+
+        document.getElementById('counter').innerHTML = (totalMoves);
+        document.getElementById('wrong').innerHTML = (wrongMoves);
+    } else {
+        timeId++;
+        totalMoves++;
+        document.getElementById('counter').innerHTML = (totalMoves);
+    }
+
+    if (timeId >= 15) clearInterval(numCardsSelected);
 }
+
